c632e3ffb49092e79a3c95fc18236a84f6e5ab1c
Vasin Dima <vasindima@ya.ru>
2026-07-08 16:50:03 +0300

    ci-cd

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.docker.operators.docker import DockerOperator
@@ -17,12 +18,12 @@ def hello_python():
     print("Привет! Это код, выполненный внутри PythonOperator!")
 
 with DAG(
-    'my_first_airflow_dag',         # Уникальный ID графа в интерфейсе
+    'my_first_airflow_dag',
     default_args=default_args,
     description='Простой тестовый DAG',
-    schedule=None,                   # Запуск только вручную (или '@daily' для ежедневного)
-    start_date=datetime(2026, 1, 1), # Дата, с которой граф начинает гипотетически существовать
-    catchup=False,                  # Не догонять прошлые даты при старте
+    schedule=None,
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
 ) as dag:
 
     task_1 = BashOperator(
@@ -34,10 +35,17 @@ with DAG(
         task_id='run_python_func',
         python_callable=hello_python,
     )
-
+    IMAGE_TAG = Variable.get("dbt_worker_image_tag", default_var="latest")
+      # CLICKHOUSE_USER: ${{ secrets.CLICKHOUSE_USER }}
+      # CLICKHOUSE_PASSWORD: ${{ secrets.CLICKHOUSE_PASSWORD }}
+      # CLICKHOUSE_DATABASE: ${{ secrets.CLICKHOUSE_DATABASE }}
+      # CLICKHOUSE_HOST: ${{ secrets.CLICKHOUSE_HOST }}
+      # CLICKHOUSE_PORT: ${{ secrets.CLICKHOUSE_PORT }}
+      # ICE_ACCESS_KEY_ID: ${{ secrets.ICE_ACCESS_KEY_ID }}
+      # ICE_SECRET_ACCESS_KEY: ${{ secrets.ICE_SECRET_ACCESS_KEY }}
     task_3 = DockerOperator(
         task_id='dbt_run',
-        image='ghcr.io/dbt-labs/dbt-postgres:latest', 
+        image=f'cr.yandex/crp00000000000000000/dbt_worker:{IMAGE_TAG}',  # Доработать
         command='dbt run --profiles-dir /usr/app --project-dir /usr/app',
         network_mode='airflow_default', 
         auto_remove='success',
